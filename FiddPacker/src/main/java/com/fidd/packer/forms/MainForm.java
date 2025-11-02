15fce63a08e86dcf4317e0759a5f6888cecb6d2d
codekrolik2 <john.amirov.83@gmail.com>
2025-11-01 20:19:25 -0800

    "Fidd Packer" - pack/unpack form - UI.

--- FiddPacker/src/main/java/com/fidd/packer/forms/MainForm.java
+++ FiddPacker/src/main/java/com/fidd/packer/forms/MainForm.java
@@ -1,6 +1,8 @@
 package com.fidd.packer.forms;
 
-import com.flower.crypt.keys.forms.AesRawKeyProvider;
+import com.flower.crypt.Cryptor;
+import com.flower.crypt.HexTool;
+import com.flower.crypt.PkiUtil;
 import com.flower.crypt.keys.forms.MultiKeyProvider;
 import com.flower.crypt.keys.forms.RsaFileKeyProvider;
 import com.flower.crypt.keys.forms.RsaPkcs11KeyProvider;
@@ -9,12 +11,19 @@ import com.flower.crypt.keys.forms.TabKeyProvider;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import javax.security.auth.x500.X500Principal;
+import java.io.File;
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -27,6 +36,14 @@ public class MainForm {
     @FXML @Nullable AnchorPane topPane;
 
     @Nullable TabKeyProvider keyProvider;
+
+    @FXML @Nullable TextField generatedAes256KeyTextField;
+    @FXML @Nullable TextField generatedAes256IvTextField;
+    @FXML @Nullable TextArea generatedCertificateTextArea;
+    @FXML @Nullable TextArea generatedPrivateKeyTextArea;
+
+    @FXML @Nullable TextField originalFolderTextField;
+    @FXML @Nullable TextField packedContentFolderTextField;
 
     public MainForm() {
         //This form is created automatically.
@@ -59,10 +76,85 @@ public class MainForm {
         MultiKeyProvider rsaTabPane = new MultiKeyProvider(mainStage, "RSA-2048",
                 List.of(rsaPkcs11KeyProvider, rsaFileKeyProvider, rsaRawKeyProvider));
 
-        AesRawKeyProvider aesRawKeyProvider = new AesRawKeyProvider();
+        //AesRawKeyProvider aesRawKeyProvider = new AesRawKeyProvider();
         return new MultiKeyProvider(mainStage, "",
-                List.of(rsaTabPane, aesRawKeyProvider));
+                List.of(rsaTabPane/*, aesRawKeyProvider*/));
     }
 
     public void quit() { checkNotNull(mainStage).close(); }
+
+    public void packFolder() {}
+
+    public void unpackFolder() {}
+
+    private @Nullable File chooseDirectory(@Nullable File initialDirectory) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        if (initialDirectory != null && initialDirectory.exists()) {
+            directoryChooser.setInitialDirectory(initialDirectory);
+        }
+        directoryChooser.setTitle("Select a Directory");
+
+        // Open the directory chooser dialog
+        return directoryChooser.showDialog(mainStage);
+    }
+
+    public void openOriginalFolder() {
+        File initialDirectory = null;
+        initialDirectory = new File(checkNotNull(originalFolderTextField).textProperty().get());
+        File directory = chooseDirectory(initialDirectory);
+        if (directory != null) {
+            checkNotNull(originalFolderTextField).textProperty().set(directory.getPath());
+        }
+    }
+
+    public void openPackedContentFolder() {
+        File initialDirectory = null;
+        initialDirectory = new File(checkNotNull(packedContentFolderTextField).textProperty().get());
+        File directory = chooseDirectory(initialDirectory);
+        if (directory != null) {
+            checkNotNull(packedContentFolderTextField).textProperty().set(directory.getPath());
+        }
+    }
+
+    public void generateAes256Key() {
+        try {
+            byte[] key = Cryptor.generateAESKeyRaw();
+            String hexEncodedKey = HexTool.bytesToHex(key);
+            checkNotNull(generatedAes256KeyTextField).textProperty().set(hexEncodedKey);
+        } catch (Exception e) {
+            LOGGER.error("AES-256 key generation error", e);
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.toString(), ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+
+    public void generateAes256Iv() {
+        try {
+            byte[] iv = Cryptor.generateAESIV();
+            String hexEncodedIv = HexTool.bytesToHex(iv);
+            checkNotNull(generatedAes256IvTextField).textProperty().set(hexEncodedIv);
+        } catch (Exception e) {
+            LOGGER.error("AES-256 IV generation error", e);
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.toString(), ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+
+    public void generateSelfSignedCertificate() {
+        try {
+            KeyPair keyPair = PkiUtil.generateRsa2048KeyPair();
+            X500Principal subject = new X500Principal("CN=Self-Generated Certificate");
+            X509Certificate certificate = PkiUtil.generateSelfSignedCertificate(keyPair, subject);
+
+            String certificateStr = PkiUtil.getCertificateAsPem(certificate);
+            String keyStr = PkiUtil.getKeyAsPem(keyPair.getPrivate());
+
+            checkNotNull(generatedCertificateTextArea).textProperty().set(certificateStr);
+            checkNotNull(generatedPrivateKeyTextArea).textProperty().set(keyStr);
+        } catch (Exception e) {
+            LOGGER.error("Self-Signed certificate generation error", e);
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.toString(), ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
 }
