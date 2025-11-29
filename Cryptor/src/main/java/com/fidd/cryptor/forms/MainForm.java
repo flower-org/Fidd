83c929348ed6fcc677272be6412810ebc3055e62
codekrolik2 <john.amirov.83@gmail.com>
2025-11-29 00:06:52 -0900

    Version bumped to 0.0.11

--- Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
+++ Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
@@ -114,7 +114,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.9", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.11", ButtonType.OK);
         alert.showAndWait();
     }
 
