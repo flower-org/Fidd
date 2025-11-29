a581dc743a8ced32246b613fc33f0b4310f1fd6d
codekrolik2 <john.amirov.83@gmail.com>
2025-11-29 00:01:25 -0900

    GitHub Actions Workflows updated

--- Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
+++ Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
@@ -114,7 +114,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.8", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.9", ButtonType.OK);
         alert.showAndWait();
     }
 
