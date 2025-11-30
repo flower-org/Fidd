1276ccd55121c6807a479bc52902908ce0749300
codekrolik2 <john.amirov.83@gmail.com>
2025-11-29 16:17:51 -0900

    Version bumped to 0.0.15

--- Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
+++ Cryptor/src/main/java/com/fidd/cryptor/forms/MainForm.java
@@ -114,7 +114,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.14", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.15", ButtonType.OK);
         alert.showAndWait();
     }
 
