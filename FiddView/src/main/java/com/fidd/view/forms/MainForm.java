9eec0d7fa0f12655c71731700b7b1b3cfbcb4d37
codekrolik2 <john.amirov.83@gmail.com>
2025-12-31 17:07:02 -0900

    Version bumped to 0.1.0

--- FiddView/src/main/java/com/fidd/view/forms/MainForm.java
+++ FiddView/src/main/java/com/fidd/view/forms/MainForm.java
@@ -39,7 +39,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Cryptor v 0.0.17", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "FiddView v 0.1.0", ButtonType.OK);
         alert.showAndWait();
     }
 
