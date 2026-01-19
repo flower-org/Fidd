701aad28cf3851db0e95f35c55a3a8aba7210492
codekrolik2 <john.amirov.83@gmail.com>
2026-01-18 18:24:02 -0900

    FiddView added to release, Version bumped to 0.2.0

--- FiddView/src/main/java/com/fidd/view/forms/MainForm.java
+++ FiddView/src/main/java/com/fidd/view/forms/MainForm.java
@@ -97,7 +97,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "FiddView v 0.1.1", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "FiddView v 0.2.0", ButtonType.OK);
         alert.showAndWait();
     }
 
