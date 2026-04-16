bfd08c78ca7a39bce5c50884b90e6def68d130fa
codekrolik2 <john.amirov.83@gmail.com>
2026-04-15 22:54:59 -0800

    version bumped to 1.2.8

--- FiddPacker/src/main/java/com/fidd/packer/forms/MainForm.java
+++ FiddPacker/src/main/java/com/fidd/packer/forms/MainForm.java
@@ -257,7 +257,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "FiddPacker v 1.2.7", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "FiddPacker v 1.2.8", ButtonType.OK);
         alert.showAndWait();
     }
 
